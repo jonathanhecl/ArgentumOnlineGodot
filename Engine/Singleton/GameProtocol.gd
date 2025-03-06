4127b84b91c27a8660fd39ff32137453e2a9e2be
brian-christopher <brian-christopher1995@outlook.com>
2025-03-06 12:48:33 -0300

    Update GameProtocol.gd

--- Engine/Singleton/GameProtocol.gd
+++ Engine/Singleton/GameProtocol.gd
@@ -8,6 +8,9 @@ static func Flush() -> PackedByteArray:
 	_writer.clear() 
 	return data
 
+static func Clear() -> void:
+	_writer.clear()
+
 static func WriteLoginExistingCharacter(username:String, password:String) -> void:
 	_writer.put_u8(Enums.ClientPacketID.LoginExistingChar)
 	Utils.PutUnicodeString(_writer, username)
@@ -15,3 +18,84 @@ static func WriteLoginExistingCharacter(username:String, password:String) -> voi
 	_writer.put_u8(0)
 	_writer.put_u8(13)
 	_writer.put_u8(0)
+	
+static func WriteThrowDice() -> void:
+	_writer.put_u8(Enums.ClientPacketID.ThrowDices)
+	
+static func WriteWalk(heading:int) -> void:
+	_writer.put_u8(Enums.ClientPacketID.Walk) 
+	_writer.put_u8(heading)
+	
+static func WriteChangeHeading(heading:int) -> void:
+	_writer.put_u8(Enums.ClientPacketID.ChangeHeading) 
+	_writer.put_u8(heading) 
+	
+static func WriteEquipItem(slot:int) -> void:
+	_writer.put_u8(Enums.ClientPacketID.EquipItem) 
+	_writer.put_u8(slot) 
+	
+static func WriteUseItem(slot:int) -> void:
+	_writer.put_u8(Enums.ClientPacketID.UseItem) 
+	_writer.put_u8(slot) 
+	
+static func WriteDoubleClick(x:int, y:int) -> void:
+	_writer.put_u8(Enums.ClientPacketID.DoubleClick) 
+	_writer.put_u8(x)
+	_writer.put_u8(y) 
+
+static func WriteLeftClick(x:int, y:int) -> void:
+	_writer.put_u8(Enums.ClientPacketID.LeftClick) 
+	_writer.put_u8(x)
+	_writer.put_u8(y) 
+	
+static func WriteWorkLeftClick(x:int, y:int, skill:int) -> void:
+	_writer.put_u8(Enums.ClientPacketID.WorkLeftClick) 
+	_writer.put_u8(x)
+	_writer.put_u8(y) 
+	_writer.put_u8(skill) 
+	
+static func WriteCommerceEnd() -> void:
+	_writer.put_u8(Enums.ClientPacketID.CommerceEnd) 
+	
+static func WriteCommerceSell(slot:int, quantity:int) -> void:
+	_writer.put_u8(Enums.ClientPacketID.CommerceSell) 
+	_writer.put_u8(slot)
+	_writer.put_16(quantity) 
+
+static func WriteCommerceBuy(slot:int, quantity:int) -> void:
+	_writer.put_u8(Enums.ClientPacketID.CommerceBuy) 
+	_writer.put_u8(slot)
+	_writer.put_16(quantity) 
+
+static func WriteBankEnd() -> void:
+	_writer.put_u8(Enums.ClientPacketID.BankEnd)  
+	
+static func WriteBankExtractItem(slot:int, quantity:int) -> void:
+	_writer.put_u8(Enums.ClientPacketID.BankExtractItem) 
+	_writer.put_u8(slot)
+	_writer.put_16(quantity) 
+
+static func WriteBankDepositItem(slot:int, quantity:int) -> void:
+	_writer.put_u8(Enums.ClientPacketID.BankDeposit) 
+	_writer.put_u8(slot)
+	_writer.put_16(quantity) 
+
+static func WriteAttack() -> void:
+	_writer.put_u8(Enums.ClientPacketID.Attack) 
+
+static func WritePickup() -> void:
+	_writer.put_u8(Enums.ClientPacketID.PickUp) 
+	
+static func WriteRequestPositionUpdate() -> void:
+	_writer.put_u8(Enums.ClientPacketID.RequestPositionUpdate) 
+
+static func WriteMeditate() -> void:
+	_writer.put_u8(Enums.ClientPacketID.Meditate) 
+	
+static func WriteWork(skill:int) -> void:
+	_writer.put_u8(Enums.ClientPacketID.Work) 
+	_writer.put_u8(skill) 
+
+static func WriteTalk(text:String) -> void:
+	_writer.put_u8(Enums.ClientPacketID.Talk) 
+	Utils.PutUnicodeString(_writer, text)
