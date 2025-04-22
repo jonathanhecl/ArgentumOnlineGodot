a29c5752b0e31bf23ba61adce9638868d8151c95
Jonathan G. Hecl <gshaxor@gmail.com>
2025-04-21 22:51:39 -0300

    Options saving

--- engine/autoload/global.gd
+++ engine/autoload/global.gd
@@ -12,3 +12,13 @@ var dialogFontSize:int:
         emit_signal("dialog_font_size_changed", _dialogFontSize)
     get:
         return _dialogFontSize
+
+func _ready() -> void:
+    # Cargar configuración guardada
+    var cfg = ConfigFile.new()
+    if cfg.load("user://options.cfg") == OK:
+        var bus = AudioServer.get_bus_index("Master")
+        var saved_db = cfg.get_value("audio", "volume_db", AudioServer.get_bus_volume_db(bus))
+        AudioServer.set_bus_volume_db(bus, saved_db)
+        var saved_fs = cfg.get_value("ui", "dialog_font_size", dialogFontSize)
+        dialogFontSize = int(saved_fs)
--- ui/hub/OptionsWindow.gd
+++ ui/hub/OptionsWindow.gd
@@ -13,6 +13,9 @@ func _ready() -> void:
 	sliderVolume.connect("value_changed", Callable(self, "_on_slider_value_changed"))
 	sliderFontSize.value = Global.dialogFontSize
 	sliderFontSize.connect("value_changed", Callable(self, "_on_slider_font_size_changed"))
+	
+	# Guardar configuración al confirmar
+	connect("confirmed", Callable(self, "_on_save_settings"))
 
 func _on_slider_value_changed(value: float) -> void:
 	var bus = AudioServer.get_bus_index("Master")
@@ -20,3 +23,11 @@ func _on_slider_value_changed(value: float) -> void:
 
 func _on_slider_font_size_changed(value: float) -> void:
 	Global.dialogFontSize = int(value)
+
+func _on_save_settings() -> void:
+	var cfg = ConfigFile.new()
+	# Cargar existente (si hay) y actualizar valores
+	cfg.load("user://options.cfg")
+	cfg.set_value("audio", "volume_db", linear_to_db(sliderVolume.value))
+	cfg.set_value("ui", "dialog_font_size", sliderFontSize.value)
+	cfg.save("user://options.cfg")
