8c39190d5005bc29119f64dddfbf3920c0a448d2
Jonathan G. Hecl <gshaxor@gmail.com>
2025-11-24 23:25:54 -0300

    feat: add `consts.gd` to centralize game constants, localized messages, and entity identifiers.

--- engine/autoload/consts.gd
+++ engine/autoload/consts.gd
@@ -3,7 +3,7 @@ extends Node
 # Versión del cliente
 const CLIENT_VERSION_MAJOR = 0
 const CLIENT_VERSION_MINOR = 13
-const CLIENT_VERSION_REVISION = 6
+const CLIENT_VERSION_REVISION = 53
 
 const MapSize = 100
 const TileSize = 32
@@ -41,50 +41,50 @@ const NumRazas = 5
 const NumCiudades = 6
 const MaxSkillpoints = 100
 
-const MessageUserHittedByUser:Dictionary[int, String] =  {
-	bCabeza : "!!{0} te ha pegado en la cabeza por {1}!!",
-	bPiernaIzquierda :  "!!{0} te ha pegado el brazo izquierdo por {1}!!",
-	bPiernaDerecha :  "!!{0} te ha pegado el brazo derecho por {1}!!",
-	bBrazoDerecho :  "!!{0} te ha pegado la pierna derecha por {1}!!",
-	bBrazoIzquierdo :  "!!{0} te ha pegado la pierna izquierda por {1}!!",
-	bTorso :  "!!{0} te ha pegado en el torso por {1}!!",
+const MessageUserHittedByUser: Dictionary[int, String] = {
+	bCabeza: "!!{0} te ha pegado en la cabeza por {1}!!",
+	bPiernaIzquierda: "!!{0} te ha pegado el brazo izquierdo por {1}!!",
+	bPiernaDerecha: "!!{0} te ha pegado el brazo derecho por {1}!!",
+	bBrazoDerecho: "!!{0} te ha pegado la pierna derecha por {1}!!",
+	bBrazoIzquierdo: "!!{0} te ha pegado la pierna izquierda por {1}!!",
+	bTorso: "!!{0} te ha pegado en el torso por {1}!!",
 }
 
-const MessageUserHittedUser:Dictionary[int, String] = {
-	bCabeza : "¡¡Le has pegado a {0} en la cabeza por por {1}!!",
-	bPiernaIzquierda :  "¡¡Le has pegado a {0} en el brazo izquierdo por {1}!!",
-	bPiernaDerecha :  "¡¡Le has pegado a {0} el brazo derecho por {1}!!",
-	bBrazoDerecho :  "¡¡Le has pegado a {0} en la pierna derecha por {1}!!",
-	bBrazoIzquierdo :  "¡¡Le has pegado a {0} en la pierna izquierda por {1}!!",
-	bTorso :  "¡¡Le has pegado a {0} en el torso por {1}!!",
+const MessageUserHittedUser: Dictionary[int, String] = {
+	bCabeza: "¡¡Le has pegado a {0} en la cabeza por por {1}!!",
+	bPiernaIzquierda: "¡¡Le has pegado a {0} en el brazo izquierdo por {1}!!",
+	bPiernaDerecha: "¡¡Le has pegado a {0} el brazo derecho por {1}!!",
+	bBrazoDerecho: "¡¡Le has pegado a {0} en la pierna derecha por {1}!!",
+	bBrazoIzquierdo: "¡¡Le has pegado a {0} en la pierna izquierda por {1}!!",
+	bTorso: "¡¡Le has pegado a {0} en el torso por {1}!!",
 }
 
-const MessageWorkRequestTarget:Dictionary[int, String] = {
+const MessageWorkRequestTarget: Dictionary[int, String] = {
 	Enums.Skill.Magia: "Haz click sobre el objetivo...",
 	Enums.Skill.Pesca: "Haz click sobre el sitio donde quieres pescar...",
 	Enums.Skill.Robar: "Haz click sobre la víctima...",
 	Enums.Skill.Domar: "Haz click sobre la criatura...",
 	Enums.Skill.Talar: "Haz click sobre el árbol...",
-	Enums.Skill.Mineria: "HHaz click sobre el yacimiento...", 
+	Enums.Skill.Mineria: "HHaz click sobre el yacimiento...",
 	Enums.Skill.Proyectiles: "Haz click sobre la victima..."
 }
 
-const ShipIds:Array[int] = [
+const ShipIds: Array[int] = [
 	# Fragatas
-	87, 190, 189, 
+	87, 190, 189,
 	# Embarcaciones normales
-	84, 85, 86, 
+	84, 85, 86,
 	# Embarcaciones ciudas
-	395, 552, 397, 560, 399, 556, 
+	395, 552, 397, 560, 399, 556,
 	# Embarcaciones reales
-	550, 553, 558, 561, 554, 557, 
+	550, 553, 558, 561, 554, 557,
 	# Embarcaciones PK
-	396, 398, 400, 
+	396, 398, 400,
 	# Embarcaciones caos
 	551, 559, 555
 ]
 
-const RaceNames:Dictionary[int, String] = {
+const RaceNames: Dictionary[int, String] = {
 	Enums.Race.Human: "Humano",
 	Enums.Race.Elf: "Elfo",
 	Enums.Race.Drow: "Drow",
@@ -92,7 +92,7 @@ const RaceNames:Dictionary[int, String] = {
 	Enums.Race.Dwarf: "Enano"
 }
 
-const ClassNames:Dictionary[int, String] = {
+const ClassNames: Dictionary[int, String] = {
 	Enums.Class.Mage: "Mago",
 	Enums.Class.Cleric: "Clérigo",
 	Enums.Class.Warrior: "Guerrero",
@@ -107,7 +107,7 @@ const ClassNames:Dictionary[int, String] = {
 	Enums.Class.Pirat: "Pirata"
 }
 
-const HomeNames:Dictionary[int, String] = {
+const HomeNames: Dictionary[int, String] = {
 	Enums.Home.Arghal: "Arghal",
 	Enums.Home.Arkhein: "Arkhein",
 	Enums.Home.Lindos: "Lindos",
